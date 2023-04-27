a785144b2e6e5914df00b321faf8f7adb4e93f4c
Cuban Tony Stark <escambray@outlook.com>
2023-04-27 12:54:19 -0400

    Update install_neural_rendering.bat

--- install_neural_rendering.bat
+++ install_neural_rendering.bat
@@ -7,7 +7,7 @@ copy run_eolian.py neural_rendering\\run_eolian.py
 xcopy /E /I carvekit neural_rendering\\carvekit
 rmdir /S/Q carvekit
 del neural_rendering\\scripts\\colmap2nerf.py
-copy colmap2nerf.py neural_rendering\\scripts\\colmap2nerf.py
+copy colmap2nerf.py /Y neural_rendering\\scripts\\colmap2nerf.py
 del run_eolian.py
 del colmap2nerf.py
 cd neural_rendering
