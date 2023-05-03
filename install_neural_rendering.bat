8deaad140e640d5b1fbbffe5df4b67e237c416ad
Cuban Tony Stark <escambray@outlook.com>
2023-05-03 11:41:47 -0400

    Update install_neural_rendering.bat

--- install_neural_rendering.bat
+++ install_neural_rendering.bat
@@ -1,8 +1,4 @@
 git clone --recursive https://github.com/cubantonystark/neural_rendering.git
-cd neural_rendering
-cmake . -B build
-cmake --build build --config RelWithDebInfo -j
-cd ..
 copy run_eolian.py neural_rendering\\run_eolian.py
 xcopy /E /I carvekit neural_rendering\\carvekit
 rmdir /S/Q carvekit
